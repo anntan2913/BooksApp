eacfd6ea3d797d3c7ba351f13b68aba12a851186
Anna Tankielun <anna.r.tankielun.3@gmail.com>
2023-03-23 21:27:15 +0100

    fix event delegation, add function filterBooks

--- src/js/script.js
+++ src/js/script.js
@@ -26,47 +26,69 @@
     }
     
     const favoriteBooks = [];
+    const filters = [];
     
     function initActions() {        
        
-        const booksImages = booksList.querySelectorAll('.book__image'); 
+        //const booksImages = booksList.querySelectorAll('.book__image'); 
+        const bookForm = document.querySelector('.filters');
         
-        for(let image of booksImages) {
+        //for(let image of booksImages) {              // e. ('image' click) => event.target.offsetParent
             
-            image.addEventListener('dblclick', function(event){
-                event.preventDefault(); // e. ('image' click) => event.target.offsetParent
-                if(event.target.offsetParent.classList.contains('book__image')){
-                    const bookId = event.target.offsetParent.getAttribute('data-id');
+        booksList.addEventListener('dblclick', function(event){ 
+            event.preventDefault(); 
+            if(event.target.offsetParent.classList.contains('book__image')){
+                const bookId = event.target.offsetParent.getAttribute('data-id');
 
-                    if(favoriteBooks.indexOf(bookId) == -1){
-                        event.target.offsetParent.classList.add('favorite');                
-                        favoriteBooks.push(bookId);
-                    } else {
-                        event.target.offsetParent.classList.remove('favorite');
-                        const bookElem = favoriteBooks.indexOf(bookId);
-                        favoriteBooks.splice(bookElem, 1);                    
-                    }
+                if(favoriteBooks.indexOf(bookId) == -1){
+                    event.target.offsetParent.classList.add('favorite');                
+                    favoriteBooks.push(bookId);
+                } else {
+                    event.target.offsetParent.classList.remove('favorite');
+                    const bookElem = favoriteBooks.indexOf(bookId);
+                    favoriteBooks.splice(bookElem, 1);                    
                 }
-                console.log('Favorite Books', favoriteBooks);                
-            });            
+            }
+            console.log('Favorite Books', favoriteBooks);                
+        });            
+        //}
+
+        bookForm.addEventListener('click', function(event){
+            //if event.target was the clicked element, which...
+            if (event.target.tagName == 'INPUT' && event.target.type == 'checkbox' && event.target.name == 'filter'){
+                console.log('checkbox-value', event.target.value);
+                if(event.target.checked == true){
+                    filters.push(event.target.value);
+                } else {
+                    const filterValue = filters.indexOf(event.target.value);
+                    filters.splice(filterValue, 1);
+                }                
+            }
+            console.log('Filter value', filters);
+            filterBooks(); 
+        });
+    }
+    
+    function filterBooks(){
+        
+        for (let book of dataSource.books){
+           let shouldBeHidden = false;
+           for (const filter of filters){
+                if(!book.details[filter]) {
+                    shouldBeHidden = true;
+                    break;
+                }                         
+                //tipp: .book__image[data-id="id-of-the-book-here"] jak w blogu-check it!
+                const bookImg = document.querySelector('.book__image[data-id="' + book.id + '"]');
+                console.log('Book Id', book.id);   
+                if(shouldBeHidden == true){
+                    bookImg.classList.add('hidden');
+                } else {                
+                    bookImg.classList.remove('hidden');
+                } console.log('bookImg', bookImg);
+            }                   
         }
     }
     render();
     initActions();  
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
