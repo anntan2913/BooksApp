5f4024933c06b6f1ee165815d91494f2760b8f58
Anna Tankielun <anna.r.tankielun.3@gmail.com>
2023-03-24 00:41:40 +0100

    add function book ratings

--- src/js/script.js
+++ src/js/script.js
@@ -22,6 +22,8 @@
             const generatedHTML = templates.book(book);
             const generatedDOM = utils.createDOMFromHTML(generatedHTML);
             booksList.appendChild(generatedDOM); 
+            const ratingBgc = determineRatingBgc(book.rating);
+            const ratingWidth = (book.rating / 10) * 100; // to percentage rate
         }
     }
     
@@ -89,6 +91,19 @@
             }                   
         }
     }
+
+    function determineRatingBgc(rating){
+
+        if (rating<6) {
+           return 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+        } else if (rating>6 && rating<=8) {
+            return 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+        } else if (rating>8 && rating<=9) {
+            return 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+        } else if (rating>9) {
+            return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+        }   
+    }    
     render();
     initActions();  
 }
